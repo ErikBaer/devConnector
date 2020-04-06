6c59b8fa9d1e7d7c4c584e1d85ccf56bbc641960
Erik Baer <erik.baer@yahoo.de>
2020-04-06 20:48:43 +0200

    connect to MongoDB Atlas

--- .history/server_20200406204439.js
+++ .history/server_20200406204439.js
@@ -3,9 +3,6 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-//COnnect to Database
-connectDB();
-
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
