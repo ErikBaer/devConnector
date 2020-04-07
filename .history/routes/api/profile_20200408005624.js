58fbe6f8ba410f45bd05f05dc582e8026d83221b
Erik Baer <erik.baer@yahoo.de>
2020-04-08 01:04:32 +0200

    create route to delete user experience entry

--- .history/routes/api/profile_20200408005624.js
+++ .history/routes/api/profile_20200408005624.js
@@ -266,20 +266,9 @@ router.put('/experience', [auth,
 
 router.delete("/experience/:exp_id", auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({
-            user: req.user.id
-        });
-        //Get remove index (check for position(index) where item.id === exp_id)
-        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
 
-        profile.experience.splice(removeIndex, 1);
-
-        await profile.save();
-
-        res.json(profile);
     } catch (err) {
-        console.err(err.message)
-        res.status(500).send('Server Error');
+
     }
 })
 
--- .history/routes/api/profile_20200408010043.js
+++ .history/routes/api/profile_20200408010043.js
@@ -269,14 +269,10 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
         const profile = await Profile.findOne({
             user: req.user.id
         });
-        //Get remove index (check for position(index) where item.id === exp_id)
+        //Get remove index
         const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
 
-        profile.experience.splice(removeIndex, 1);
-
-        await profile.save();
-
-        res.json(profile);
+        profile.experience
     } catch (err) {
         console.err(err.message)
         res.status(500).send('Server Error');
